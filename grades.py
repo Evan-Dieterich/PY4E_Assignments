3886518535a23c5abcbcf87de8557c8f41db540f
Evan Dieterich <75453581+SweetEvan@users.noreply.github.com>
2021-01-25 14:11:34 -0700

    Update grades.py

--- grades.py
+++ grades.py
@@ -1,4 +1,4 @@
-PY4E Assignment 3.3 Solution - Evan Dieterich
+#PY4E Assignment 3.3 Solution - Evan Dieterich
 '''
 Write a program to prompt for a score between 0.0 and 1.0. If the score is out of range, print an error. 
 If the score is between 0.0 and 1.0, print a grade using the following table:
